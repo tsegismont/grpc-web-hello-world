d6174400283ffb2ba80d5e31f1b173cffcf08293
Thomas Segismont <tsegismo@redhat.com>
2024-02-15 11:08:50 +0100

    Implement headers/trailers echo

--- src/main/java/io/vertx/grpcweb/MyServerInterceptor.java
+++ src/main/java/io/vertx/grpcweb/MyServerInterceptor.java
@@ -1,26 +1,27 @@
 package io.vertx.grpcweb;
 
 import io.grpc.*;
+import io.grpc.Metadata.Key;
+
+import java.util.Set;
 
 class MyServerInterceptor implements ServerInterceptor {
-
-  // TODO
+  private static final Set<Key<?>> HEADERS_KEY_SET = Set.of(Key.of("x-grpc-test-echo-initial", Metadata.ASCII_STRING_MARSHALLER));
+  private static final Set<Key<?>> TRAILERS_KEY_SET = Set.of(Key.of("x-grpc-test-echo-trailing-bin", Metadata.BINARY_BYTE_MARSHALLER));
 
   @Override
   public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata metadata, ServerCallHandler<ReqT, RespT> next) {
-    System.out.println("MyServerInterceptor.interceptCall");
-    System.out.println("call = " + call + ", metadata = " + metadata + ", next = " + next);
     return next.startCall(new ForwardingServerCall.SimpleForwardingServerCall<>(call) {
 
       @Override
       public void sendHeaders(Metadata headers) {
-        System.out.println("MyServerInterceptor.sendHeaders");
-        System.out.println("headers = " + headers);
+        headers.merge(metadata, HEADERS_KEY_SET);
         super.sendHeaders(headers);
       }
 
       @Override
       public void close(Status status, Metadata trailers) {
+        trailers.merge(metadata, TRAILERS_KEY_SET);
         super.close(status, trailers);
       }
     }, metadata);
