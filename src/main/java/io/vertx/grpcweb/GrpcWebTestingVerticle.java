960e5c09cbb6eeb1e9537e5a7dbe27d748a1a135
Thomas Segismont <tsegismo@redhat.com>
2024-02-22 17:59:46 +0100

    Integration test with Vert.x gRPC Web (text protocol)

--- src/main/java/io/vertx/grpcweb/GrpcWebTestingVerticle.java
+++ src/main/java/io/vertx/grpcweb/GrpcWebTestingVerticle.java
@@ -7,9 +7,13 @@ import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.CorsHandler;
 import io.vertx.grpc.server.GrpcServer;
 import io.vertx.grpc.server.GrpcServiceBridge;
 import io.vertx.grpcweb.VertxUnimplementedServiceGrpcServer.UnimplementedServiceApi;
+
+import java.util.Set;
 
 public class GrpcWebTestingVerticle extends AbstractVerticle {
 
@@ -20,21 +24,31 @@ public class GrpcWebTestingVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) {
+
+    Router router = Router.router(vertx);
+
+    router.route().handler(CorsHandler.create()
+      .exposedHeader("x-header-text-key,x-header-bin-key-bin,x-trailer-text-key,x-trailer-bin-key-bin,x-error-trailer,grpc-status,grpc-message"));
+
     GrpcServer grpcServer = GrpcServer.server(vertx);
 
-    ServerServiceDefinition serviceDefinition = ServerInterceptors.intercept(new TestServiceImpl(context), new MyServerInterceptor());
+    ServerServiceDefinition testServiceDef = ServerInterceptors.intercept(new TestServiceImpl(context), new MyServerInterceptor());
 
-    GrpcServiceBridge.bridge(serviceDefinition).bind(grpcServer);
+    GrpcServiceBridge.bridge(testServiceDef).bind(grpcServer);
+
+    router.post("/" + TestServiceGrpc.SERVICE_NAME + "/*").handler(rc -> grpcServer.handle(rc.request()));
 
     UnimplementedServiceApi unimplementedServiceStub = new UnimplementedServiceApi() {
     };
     unimplementedServiceStub.bindAll(grpcServer);
 
+    router.post("/" + UnimplementedServiceGrpc.SERVICE_NAME + "/*").handler(rc -> grpcServer.handle(rc.request()));
+
     HttpServer server = vertx.createHttpServer(new HttpServerOptions());
 
     server
-      .requestHandler(grpcServer)
-      .listen(9090)
+      .requestHandler(router)
+      .listen(8080)
       .<Void>mapEmpty()
       .onComplete(startPromise);
   }
